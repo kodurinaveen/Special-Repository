98d4b07afc4129e141438801208aad02a65d23d5
kodurinaveen <kumarkn400@gmail.com>
2023-08-19 19:25:45 +0530

    Naveen : new test case has been added.

---
+++ maven-archetype-quickstart2/src/main/java/newtestNGpackage/Githubtestaddingclass.java
@@ -0,0 +1,10 @@
+package newtestNGpackage;
+
+public class Githubtestaddingclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("are you there");
+	}
+
+}
